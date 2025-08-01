2ed7ff3a05679673787629bc4836f5574c469a30
Krish Makadiya <krishmakadiya2005@gmail.com>
2025-08-01 21:19:24 +0530

    Update MagnetHelper.js

--- src/components/Magnet/MagnetHelper.js
+++ src/components/Magnet/MagnetHelper.js
@@ -5,7 +5,7 @@ import "./Magnet.css";
 const MagnetHelper = () => {
     return (
         <Magnet padding={120} disabled={false} magnetStrength={5}>
-            <a href="https://drive.google.com/file/d/1PjQ9Cg_ln9XMkVFzojh9Gm7FueO1S23q/view?usp=drive_link" className="btn-shine" target="_blank">
+            <a href="https://drive.google.com/file/d/1jxpl9hWgLEKKsAyVTyTF9b5EZG2aWHAx/view?usp=sharing" className="btn-shine" target="_blank">
                 Download Resume
             </a>
         </Magnet>
